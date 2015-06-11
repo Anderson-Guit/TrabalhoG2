1cf590afc9a0d2172fcb87616a41c00f0708b073
Anderson Guit <anderson_guit@live.com>
2015-06-10 21:31:31 -0300

    subindo a aplicação

---
+++ Conn/MySqlConn.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conn
+{
+    class MySqlConn
+    {
+        private static MySqlConnection conn = new MySqlConnection();
+        private static MySqlCommand cmd = new MySqlCommand();
+        private static MySqlDataReader dr;
+
+        public static string StrConn = "Server=localhost" +
+                                       ";Port=3306" +
+                                       ";Database=TrabalhoG2" +
+                                       ";Uid=root" +
+                                       ";Pwd=naa";
+
+        public static bool Connect()
+        {
+
+            if (conn.State == System.Data.ConnectionState.Closed)
+            {
+                conn.ConnectionString = StrConn;
+                conn.Open();
+            }
+
+            return true;
+        }
+
+        public static bool Close()
+        {
+            conn.Close();
+            return true;
+        }
+
+        public static bool CommandPersist(MySqlCommand pCmd)
+        {
+            Connect();
+
+            if (dr != null && !dr.IsClosed)
+                dr.Close();
+
+            pCmd.Connection = conn;
+            pCmd.ExecuteNonQuery();
+            return true;
+        }
+
+        public static MySqlDataReader Get(string pSql)
+        {
+            Connect();
+            if (dr != null && !dr.IsClosed)
+                dr.Close();
+
+            cmd = new MySqlCommand(pSql, conn);
+            dr = cmd.ExecuteReader();
+
+            return dr;
+        }
+
+    }
+
+}
