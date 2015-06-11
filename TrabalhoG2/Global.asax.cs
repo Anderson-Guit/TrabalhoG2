654ded5e8f50c2151ba07ed71e2d8639b0a1e8d3
Anderson Guit <anderson_guit@live.com>
2015-06-10 21:33:25 -0300

    alteraçao no global asa

--- TrabalhoG2/Global.asax.cs
+++ TrabalhoG2/Global.asax.cs
@@ -16,7 +16,7 @@ namespace TrabalhoG2
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            //teste
+            
         }
     }
 }
