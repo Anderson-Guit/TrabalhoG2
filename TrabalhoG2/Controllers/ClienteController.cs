687842120ac71271d4a3c96b4969d47e9f060cb2
Douglas Deon <douglas_deon@hotmail.com>
2015-06-24 01:39:57 -0300

    controllers

---
+++ TrabalhoG2/Controllers/ClienteController.cs
@@ -0,0 +1,59 @@
+﻿using Data;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabalhoG2.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateClienteView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateClienteView(Cliente pCliente)
+        {
+            ClienteRepository nova = new ClienteRepository();
+            nova.Create(pCliente);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult EditClienteView(int pId)
+        {
+            var clientes = ClienteRepository.GetOne(pId);
+            return View(clientes);
+        }
+
+        [HttpPost]
+        public ActionResult EditClienteView(Cliente cliente)
+        {
+            ClienteRepository edit = new ClienteRepository();
+            edit.Update(cliente);
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult DeleteClienteView(int pId)
+        {
+            ClienteRepository exclui = new ClienteRepository();
+            exclui.Delete(pId);
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
---
+++ TrabalhoG2/Controllers/OrdemServicoController.cs
@@ -0,0 +1,58 @@
+﻿using Data;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabalhoG2.Controllers
+{
+    public class OrdemServicoController : Controller
+    {
+        // GET: OrdemServico
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateOsView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateOsView(OrdemServico pOs)
+        {
+            OrdemServicoRepository nova = new OrdemServicoRepository();
+            nova.Create(pOs);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult EditOsView(int pId)
+        {
+            var ordens = ClienteRepository.GetOne(pId);
+            return View(ordens);
+        }
+
+        [HttpPost]
+        public ActionResult EditOsView(OrdemServico ordem)
+        {
+            OrdemServicoRepository edit = new OrdemServicoRepository();
+            edit.Update(ordem);
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult DeleteOsView(int pId)
+        {
+            OrdemServicoRepository exclui = new OrdemServicoRepository();
+            exclui.Delete(pId);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
