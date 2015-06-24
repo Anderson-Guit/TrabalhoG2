7f71a948c21d703cd2588235dc3eef97b098fd1b
Anderson Guit <anderson_guit@live.com>
2015-06-24 00:10:53 -0300

    Alterações no repository de OS

--- Repository/OrdemServicoRepository.cs
+++ Repository/OrdemServicoRepository.cs
@@ -17,7 +17,7 @@ namespace Repository
             StringBuilder sql = new StringBuilder();
             MySqlCommand cmd = new MySqlCommand();
 
-            sql.Append("INSERT INTO OrdemServico (Equipamento, Marca, Modelo, NumeroSerie, Defeito, Servico, Local, Observacoes, Status)");
+            sql.Append("INSERT INTO OrdemServico (Equipamento, Marca, Modelo, NumeroSerie, Defeito, Servico, Local_, Observacoes, Status_)");
             sql.Append("VALUES(@Equipamento, @Marca, @Modelo, @NumeroSerie, @Defeito, @Servico, @Local, @Observacoes, @Status)");
 
             cmd.Parameters.AddWithValue("@Equipamento", (pOS.Equipamento));
@@ -39,7 +39,7 @@ namespace Repository
             StringBuilder sql = new StringBuilder();
             MySqlCommand cmd = new MySqlCommand();
 
-            sql.Append("UPDATE OrdemServico SET Equipamento=@Equipamento, Marca=@Marca, Modelo=@Modelo, NumeroSerie=@NumeroSerie, Defeito=@Defeito, Servico=@Servico, Local=@Local, Observacoes=@Observacoes, Status=@Status");
+            sql.Append("UPDATE OrdemServico SET Equipamento=@Equipamento, Marca=@Marca, Modelo=@Modelo, NumeroSerie=@NumeroSerie, Defeito=@Defeito, Servico=@Servico, Local=@Local_, Observacoes=@Observacoes, Status=@Status_");
             sql.Append("WHERE IdOS=" + pOS.IdOS);
 
             cmd.Parameters.AddWithValue("@Equipamento", (pOS.Equipamento));
@@ -81,16 +81,16 @@ namespace Repository
 
             while (dr.Read())
             {
-                os.IdOS = (int)dr["IdOs"];
+                os.IdOS = (int)dr["IdOS"];
                 os.Equipamento = (String)dr["Equipamento"];
                 os.Marca = (String)dr["Marca"];
                 os.Modelo = (String)dr["Modelo"];
                 os.NumeroSerie = (String)dr["NumeroSerie"];
                 os.Defeito = (String)dr["Defeito"];
                 os.Servico = (String)dr["Servico"];
-                os.Local = (String)dr["Local"];
+                os.Local = (String)dr["Local_"];
                 os.Observacoes = (String)dr["Observacoes"];
-                os.Status = (String)dr["Status"];
+                os.Status = (String)dr["Status_"];
             }
             return os;
         }
@@ -117,9 +117,9 @@ namespace Repository
                         NumeroSerie = (String)dr["NumeroSerie"],
                         Defeito = (String)dr["Defeito"],
                         Servico = (String)dr["Servico"],
-                        Local = (String)dr["Local"],
+                        Local = (String)dr["Local_"],
                         Observacoes = (String)dr["Observacoes"],
-                        Status = (String)dr["Status"]
+                        Status = (String)dr["Status_"]
                     });
             }
             return os;
