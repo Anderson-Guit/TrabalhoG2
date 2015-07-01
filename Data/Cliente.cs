6208dd48041432b203708ab4247b960f927d4872
Douglas Deon <douglas_deon@hotmail.com>
2015-06-30 21:22:09 -0300

    Validações 1

--- Data/Cliente.cs
+++ Data/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,37 @@ namespace Data
 {
     public class Cliente
     {
+        
         public int IdCliente { get; set; }
+
+        [Display(Name = "Insira o Nome", Description = "Informe o Nome do Cliente.")]
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage =
+            "Números e caracteres especiais não são permitidos no nome.")]
         public String Nome { get; set; }
 
         //[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+         [Display(Name = "Telefone", Description = "Informe o telefone do Cliente.")]
+        [Required (ErrorMessage="Telefone é obrigatório")]
+        
         public String Telefone { get; set; }
+         [Display(Name = "Endereço", Description = "Informe o Endereço do Cliente.")]
+         [Required(ErrorMessage = "Endereço é obrigatório")]
         public String Endereco { get; set; }
+         [Display(Name = "Bairro", Description = "Informe o Bairro do Cliente.")]
+         [Required(ErrorMessage = "Bairro é obrigatório")]
         public String Bairro { get; set; }
+         [Display(Name = "Cidade", Description = "Informe a Cidade do Cliente.")]
+         [Required(ErrorMessage = "Cidade é obrigatório")]
         public String Cidade { get; set; }
+         [Display(Name = "Estado", Description = "Informe o Estado do Cliente.")]
+         [Required(ErrorMessage = "Estado é obrigatório")]
         public String Estado { get; set; }
+         [Display(Name = "País", Description = "Informe o País do Cliente.")]
+         [Required(ErrorMessage = "País é obrigatório")]
         public String Pais { get; set; }
+         [Display(Name = "Cpf", Description = "Informe o Cpf do Cliente.")]
+         [Required(ErrorMessage = "Cpf é obrigatório")]
         public String CPF { get; set; }
 
         public Cliente()
